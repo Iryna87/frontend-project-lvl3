59aaa7fb67721e6c5b16899992c18631cbdb0787
Irena <iryna.morozova87@gmail.com>
2021-03-02 19:57:55 +0100

    init.js

--- src/init.js
+++ src/init.js
@@ -3,6 +3,7 @@
 import Example from './Example.js';
 
 export default () => {
+  // eslint-disable-next-line no-undef
   const element = document.getElementById('point');
   const obj = new Example(element);
   obj.init();
