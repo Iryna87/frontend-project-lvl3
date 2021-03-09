20d8ff97f88cb05570e7ad65f6015edabdcb8632
Irena <iryna.morozova87@gmail.com>
2021-03-09 20:32:32 +0100

    jsdom

--- src/init.js
+++ src/init.js
@@ -5,6 +5,7 @@ import Example from './Example.js';
 export default () => {
   // eslint-disable-next-line no-undef
   const element = document.querySelector('button');
+  console.log(element);
   const obj = new Example(element);
   obj.init();
 };
