a35c633b2806960fb638d49d0bba39e51c136cd4
Irena <iryna.morozova87@gmail.com>
2021-03-09 21:35:26 +0100

    rss.test

--- __tests__/rss.test.js
+++ __tests__/rss.test.js
@@ -1,6 +1,19 @@
-import { test, expect } from '@jest/globals';
-import rss from '../src/index.js';
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import init from '../src/init';
 
-test('rss', () => {
-  expect(rss()).toEqual('Add');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+beforeEach(async () => {
+  const pathToHtml = path.resolve(__dirname, '__fixtures__/index.html');
+  const html = await fs.readFile(pathToHtml, 'utf8');
+  // eslint-disable-next-line no-undef
+  document.body.innerHTML = html;
+});
+
+test('init', () => {
+  init();
+  expect(true).toBeDefined();
 });
--- src/init.js
+++ src/init.js
@@ -1,11 +1,9 @@
 // @ts-check
-
 import Example from './Example.js';
 
 export default () => {
   // eslint-disable-next-line no-undef
-  const element = document.querySelector('button');
-  console.log(element);
+  const element = document.getElementsByTagName('button');
   const obj = new Example(element);
   obj.init();
 };
