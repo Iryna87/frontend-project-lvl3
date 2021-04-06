a5375ffbf6cf9e7b8da9f85b5f9c9df8e42ac602
Irena <iryna.morozova87@gmail.com>
2021-04-06 21:03:37 +0200

    validation

--- src/init.js
+++ src/init.js
@@ -7,9 +7,20 @@ import DOMParser from 'dom-parser';
 import render from './view.js';
 import parseFeed from './parse.js';
 
-const schema = yup.object().shape({
-  url: yup.string().url(),
-});
+const arr = [];
+
+const validate = (str) => {
+  const schema = yup.string()
+    .required()
+    .url()
+    .notOneOf(arr);
+  try {
+    schema.validateSync(str);
+    return {};
+  } catch (err) {
+    return err;
+  }
+};
 
 export default () => {
   let i = 1;
@@ -38,6 +49,7 @@ export default () => {
     title: document.getElementById('title'),
     description: document.getElementById('description'),
     feedback: document.querySelector('.feedback'),
+    feedElements: document.querySelectorAll('.feed'),
   };
 
   const watchedState = onChange(state, (path, value) => render(path, value, elements));
@@ -46,98 +58,86 @@ export default () => {
   form.addEventListener('submit', async (e) => {
     e.preventDefault();
     const { input } = elements;
-    const url = input.value;
+    const url = input.value.trim();
     watchedState.searchForm.valid = '';
     watchedState.searchForm.url = url;
-    schema
-      .validate({ url: watchedState.searchForm.url })
-      .then(() => {
-        axios.get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}&disableCache=true`)
-          .then((response) => {
-            const domparser = new DOMParser();
-            const parsedFeed = parseFeed(domparser.parseFromString(response.data.contents, 'text/xml'), i);
-            const validation = parsedFeed.feedParsed.title;
-            const arr = [];
-            let doubleValidation = '';
-            const feedElements = document.querySelectorAll('.feed');
-            Array.from(feedElements).forEach((feed) => {
-              arr.push(feed.textContent.trim());
-              if (_.includes(arr, validation.trim())) {
-                doubleValidation = 'key2';
-              }
-            });
-            if (doubleValidation !== '') {
-              console.log(doubleValidation);
-              watchedState.searchForm.errors = doubleValidation;
-            } else {
-              watchedState.searchForm.feeds = parsedFeed.feedParsed;
-              watchedState.searchForm.posts = parsedFeed.postsParsed;
-              watchedState.searchForm.valid = 'key3';
-              i += 1;
-              const posts = document.querySelector('.posts');
-              const buttons = posts.querySelectorAll('.btn');
-              Array.from(buttons).forEach((btn) => {
-                btn.addEventListener('click', () => {
-                  const id = parseInt(btn.getAttribute('idpost'), 10);
-                  const { description } = parsedFeed.postsParsed.find((post) => post.idPost === id);
-                  const { title } = parsedFeed.postsParsed.find((post) => post.idPost === id);
-                  const modalUrl = parsedFeed.postsParsed.find((post) => post.idPost === id).url;
-                  watchedState.UI.modalPostTitle = title;
-                  watchedState.UI.modalPostDescription = description;
-                  watchedState.UI.modalPostUrl = modalUrl;
-                });
-              });
-              const dismissBtns = document.querySelectorAll('button[data-dismiss="modal"]');
-              const readBtn = document.querySelector('button[data="read"]');
-              const description = document.getElementById('description');
-              readBtn.addEventListener('click', () => {
-                const href = description.getAttribute('href');
-                window.open(href);
-                watchedState.UI.modalModus = 'off';
-                watchedState.UI.modalModus = 'on';
-              });
-              Array.from(dismissBtns).forEach((dismissBtn) => {
-                dismissBtn.addEventListener('click', () => {
-                  watchedState.UI.modalModus = 'off';
-                  watchedState.UI.modalModus = 'on';
-                });
-              });
-            }
-          })
-          .then(() => {
-            window.setTimeout(function getData() {
-              axios.get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}&disableCache=true`)
-                .then((response1) => {
-                  const domparser = new DOMParser();
-                  const parsedFeed1 = parseFeed(domparser.parseFromString(response1.data.contents, 'text/xml'), i);
-                  const newArr = parsedFeed1.postsParsed;
-                  const posts = document.getElementsByTagName('a');
-                  const oldArr = [];
-                  Array.from(posts).forEach((post) => {
-                    if (post.href.slice(0, 6) === url.slice(0, 6)) {
-                      oldArr.push({
-                        title: post.textContent.trim(),
-                      });
-                    }
-                  });
-                  const result = newArr.filter((elm) => {
-                    const a = !oldArr.map((elm1) => elm1.title.trim()).includes(elm.title.trim());
-                    return a;
-                  });
-                  watchedState.searchForm.posts = result;
-                })
-                .catch((err) => {
-                  watchedState.searchForm.errors = err;
-                });
-              setTimeout(getData, 5000);
-            }, 5000);
-          })
-          .catch((err) => {
-            watchedState.searchForm.errors = err;
-          });
-      })
-      .catch(() => {
+    const errors = validate(watchedState.searchForm.url);
+    arr.push(url);
+    if (!_.isEmpty(errors)) {
+      if (_.includes('ValidationError: this must be a valid URL', errors)) {
         watchedState.searchForm.errors = 'key4';
-      });
+      } else {
+        watchedState.searchForm.errors = 'key2';
+      }
+    } else {
+      axios.get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}&disableCache=true`)
+        .then((response) => {
+          const domparser = new DOMParser();
+          const parsedFeed = parseFeed(domparser.parseFromString(response.data.contents, 'text/xml'), i);
+          watchedState.searchForm.feeds = parsedFeed.feedParsed;
+          watchedState.searchForm.posts = parsedFeed.postsParsed;
+          watchedState.searchForm.valid = 'key3';
+          i += 1;
+          const posts = document.querySelector('.posts');
+          const buttons = posts.querySelectorAll('.btn');
+          Array.from(buttons).forEach((btn) => {
+            btn.addEventListener('click', () => {
+              const id = parseInt(btn.getAttribute('idpost'), 10);
+              const { description } = parsedFeed.postsParsed.find((post) => post.idPost === id);
+              const { title } = parsedFeed.postsParsed.find((post) => post.idPost === id);
+              const modalUrl = parsedFeed.postsParsed.find((post) => post.idPost === id).url;
+              watchedState.UI.modalPostTitle = title;
+              watchedState.UI.modalPostDescription = description;
+              watchedState.UI.modalPostUrl = modalUrl;
+            });
+          });
+          const dismissBtns = document.querySelectorAll('button[data-dismiss="modal"]');
+          const readBtn = document.querySelector('button[data="read"]');
+          const description = document.getElementById('description');
+          readBtn.addEventListener('click', () => {
+            const href = description.getAttribute('href');
+            window.open(href);
+            watchedState.UI.modalModus = 'off';
+            watchedState.UI.modalModus = 'on';
+          });
+          Array.from(dismissBtns).forEach((dismissBtn) => {
+            dismissBtn.addEventListener('click', () => {
+              watchedState.UI.modalModus = 'off';
+              watchedState.UI.modalModus = 'on';
+            });
+          });
+        })
+        .then(() => {
+          window.setTimeout(function getData() {
+            axios.get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}&disableCache=true`)
+              .then((response1) => {
+                const domparser = new DOMParser();
+                const parsedFeed1 = parseFeed(domparser.parseFromString(response1.data.contents, 'text/xml'), i);
+                const newArr = parsedFeed1.postsParsed;
+                const posts = document.getElementsByTagName('a');
+                const oldArr = [];
+                Array.from(posts).forEach((post) => {
+                  if (post.href.slice(0, 6) === url.slice(0, 6)) {
+                    oldArr.push({
+                      title: post.textContent.trim(),
+                    });
+                  }
+                });
+                const result = newArr.filter((elm) => {
+                  const a = !oldArr.map((elm1) => elm1.title.trim()).includes(elm.title.trim());
+                  return a;
+                });
+                watchedState.searchForm.posts = result;
+              })
+              .catch((err) => {
+                watchedState.searchForm.errors = err;
+              });
+            setTimeout(getData, 5000);
+          }, 5000);
+        })
+        .catch((err) => {
+          watchedState.searchForm.errors = err;
+        });
+    }
   });
 };
