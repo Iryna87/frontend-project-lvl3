1026afe4604069d28fee0ae1443a65e2ab42994a
Irena <iryna.morozova87@gmail.com>
2021-06-04 22:27:34 +0200

    updateRss

--- src/updateRssFeeds.js
+++ src/updateRssFeeds.js
@@ -4,23 +4,23 @@ import _ from 'lodash';
 import parseFeed from './parse.js';
 import proxifyURL from './proxifyURL.js';
 
-export default (watchedState, state) => {
-  setTimeout(function getData() {
-    state.feeds.forEach((feed) => {
-      axios.get(proxifyURL(feed.url))
-        .then((response) => {
-          const parsedFeed = parseFeed(response.data.contents);
-          const postsNew = parsedFeed.postsParsed;
-          const posts = _.flatten(state.posts).filter((post) => post.idFeed === feed.idFeed);
-          const result = postsNew.filter((elm) => {
-            const a = !posts.map((elm1) => elm1.title.trim()).includes(elm.title.trim());
-            return a;
-          });
-          if (result.length !== 0) {
-            watchedState.posts.unshift(result);
-          }
+export default (watchedState) => {
+  const getData = () => {
+    const promises = watchedState.feeds.map((feed) => axios.get(proxifyURL(feed.url))
+      .then((response) => {
+        const parsedFeed = parseFeed(response.data.contents);
+        const postsNew = parsedFeed.postsParsed;
+        const posts = _.flatten(watchedState.posts).filter((post) => post.idFeed === feed.idFeed);
+        const result = postsNew.filter((elm) => {
+          const a = !posts.map((elm1) => elm1.title.trim()).includes(elm.title.trim());
+          return a;
         });
-      setTimeout(getData, 5000);
-    });
-  }, 5000);
+        if (result.length !== 0) {
+          watchedState.posts.unshift(result);
+        }
+        return null;
+      }));
+    return Promise.all(promises).then(() => setTimeout(getData, 5000));
+  };
+  setTimeout(getData, 5000);
 };
